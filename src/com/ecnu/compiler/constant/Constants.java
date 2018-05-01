2b9d0b2bf96a31df64e65cee1ba6c1762edaa895
zhaochen <zhaochen704168041@qq.com>
2018-05-01 12:27:52 +0800

    更新部分正则表达式

--- src/com/ecnu/compiler/constant/Constants.java
+++ src/com/ecnu/compiler/constant/Constants.java
@@ -14,14 +14,14 @@ public class Constants {
      * @author ZhaoChen
      */
     //标识符
-    public static final String IDENTIFIER = "example";
+    public static final String JAVA_IDENTIFIER = "^[A-Za-z_]+[A-Za-z_0-9]*$";
     //常量
     public static final String CONSTANT = "example";
     //保留字
     public static final String RESERVED = "example";
     //注释
-    public static final String COMMENT = "example";
+    public static final String JAVA_COMMENT = "^(//.*?\\n)|(/\\*(.|\\n)*?\\*/)$";
     //运算符
-    public static final String OPERATOR = "example";
+    public static final String JAVA_OPERATOR = "^[\\+\\-\\*/%]$";
     //Testtest
 }
